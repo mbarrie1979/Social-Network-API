3ce977ea52554bdcaffa2292bbe50003cd2832d8
Mark Barrie <mark.a.barrie@gmail.com>
2024-04-28 17:26:09 -0400

    seed data for users complete

--- models/User.js
+++ models/User.js
@@ -1,40 +1,38 @@
 const { Schema, model } = require('mongoose');
 
 // User model/schema
-const userSchema = new Schema(
-    {
-        userName: {
-            type: String,
-            required: true,
-            maxlength: 50,
-            unique: true
-        },
-        email: {
-            type: String,
-            required: true,
-            maxlength: 50,
-        },
-        thoughts: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }],
-        reactions: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Reaction'
-        }],
-        friends: [{
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }],
-
-        toJSON: {
-            getters: true,
-            virtuals: true
-        },
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        maxlength: 50,
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    reactions: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Reaction'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: {
+        getters: true,
+        virtuals: true
     }
-);
+});
 
-// For friendcount
+// Virtual for friend count
 userSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
